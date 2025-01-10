892337a2317966411025a331bbf8a0c393798aa7
PixelPeely <jeremiahrugroden@gmail.com>
2025-01-10 16:52:58 -0600

    Initial Commit (Drivetrain Only)

---
+++ Jarvis 2025/src/main/java/frc/robot/Util/SparkMaxSetter.java
@@ -0,0 +1,38 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Util;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import frc.robot.Util.PIDDisplay.PIDSetter;
+
+/** Add your docs here. */
+public class SparkMaxSetter implements PIDSetter {
+
+    private List<SparkMaxConfig> configs;
+    private Gains gains;
+    
+    public SparkMaxSetter(SparkMaxConfig... configs){
+        this.configs.addAll(Arrays.asList(configs));
+    }
+
+    public void addConfigurator(SparkMaxConfig config) {
+        configs.add(config);
+    }
+
+    @Override
+    public void setPID(Gains gains) {
+        this.gains = gains;
+        configs.forEach(config -> config.closedLoop.pidf(gains.P, gains.I, gains.D, gains.F));
+    }
+
+    @Override
+    public Gains getPID() {
+        return gains;
+    }
+}
